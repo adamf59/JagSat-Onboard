70f3e34b0f754a50d043e50892f118491063b416
adamf59 <af.frank.2001@gmail.com>
2019-02-08 12:37:19 -0500

    cleaned up some fragments

--- jagsat-onboard/src/main/java/adamf59/SystemHostController/IO/GPIO.java
+++ jagsat-onboard/src/main/java/adamf59/SystemHostController/IO/GPIO.java
@@ -7,11 +7,6 @@ package adamf59.SystemHostController.IO;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinAnalogOutput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 
 import adamf59.SystemHostController.SystemHost;
 
