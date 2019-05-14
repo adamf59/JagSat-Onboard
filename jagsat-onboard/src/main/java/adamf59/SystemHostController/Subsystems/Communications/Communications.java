ef01c8ba7a39f89f7de5b3cd48a6317c272f8a3f
adamf59 <af.frank.2001@gmail.com>
2019-05-13 23:36:43 -0400

    deploy@11:36PMon5.13.19

--- jagsat-onboard/src/main/java/adamf59/SystemHostController/Subsystems/Communications/Communications.java
+++ jagsat-onboard/src/main/java/adamf59/SystemHostController/Subsystems/Communications/Communications.java
@@ -104,7 +104,7 @@ public class Communications extends Subsystem {
         Console.printInfo("Transmitting (TX): " + data);
 
         try {
-            serial.write(data + "\r");
+            serial.write(data);
 
         } catch (SerialPortException e) {
 
