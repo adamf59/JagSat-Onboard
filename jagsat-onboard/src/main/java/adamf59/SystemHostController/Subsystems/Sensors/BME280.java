3838bcbcdf2f78d0c8f103db35482b8ba5913526
adamf59 <af.frank.2001@gmail.com>
2019-05-26 16:00:37 -0400

    bus 2 change

--- jagsat-onboard/src/main/java/adamf59/SystemHostController/Subsystems/Sensors/BME280.java
+++ jagsat-onboard/src/main/java/adamf59/SystemHostController/Subsystems/Sensors/BME280.java
@@ -16,7 +16,7 @@ public class BME280
     public static void main() throws Exception
     {
         // Create I2C bus
-        I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+        I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_2);
         // Get I2C device, BME280 I2C address is 0x76(108)
         I2CDevice device = bus.getDevice(0x77);
         
