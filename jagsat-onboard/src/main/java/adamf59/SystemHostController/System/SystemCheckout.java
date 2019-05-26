2dc2662052631b52d4ececa55994b009d53a8f9d
adamf59 <af.frank.2001@gmail.com>
2019-05-26 15:34:06 -0400

    deploy@3:32PMon5.26.19

--- jagsat-onboard/src/main/java/adamf59/SystemHostController/System/SystemCheckout.java
+++ jagsat-onboard/src/main/java/adamf59/SystemHostController/System/SystemCheckout.java
@@ -11,43 +11,58 @@ import adamf59.Core.Command;
 import adamf59.SystemHostController.SystemHost;
 import adamf59.SystemHostController.Subsystems.Communications.Communications;
 import adamf59.SystemHostController.Subsystems.Communications.Transmit;
+import adamf59.SystemHostController.Subsystems.Sensors.BME280;
 
 public class SystemCheckout extends Command {
 
     /**
-     * Command responsible for testing all subsystems to ensure they are working properly
+     * Command responsible for testing all subsystems to ensure they are working
+     * properly
      */
     public SystemCheckout() {
         super("SYSTEM_CHECKOUT");
     }
 
-    
     @Override
     public void init() {
         Console.printInfo("====System Check====");
         Console.printOk("Disabling Subsystems Temporarily...");
-            SystemHost.getAvionics().suspendSubsystem();
-            SystemHost.getCommunications().suspendSubsystem();
+        SystemHost.getAvionics().suspendSubsystem();
+        SystemHost.getCommunications().suspendSubsystem();
     }
 
     @Override
     public void execute() {
 
         Console.printInfo("Checking Serial TXRX Connections...");
-       SystemHost.getSchedulerService().scheduleTask(new Transmit("AT"), SchedulerService.PRIORITY_LOW);
-       Console.printInfo("Awaiting new data");
+        SystemHost.getSchedulerService().scheduleTask(new Transmit("AT"), SchedulerService.PRIORITY_LOW);
+        Console.printInfo("Awaiting new data");
 
-       sleep(2000);
-            if(SystemHost.getCommunications().getLastResponse().contains("OK")) {
-                Console.printOk("Serial TXRX Test Passed. No issues detected.");
-            } else {
-                Console.printErr("Serial TXRX Test Failed. Data recieved was not expected. I got: " + SystemHost.getCommunications().getLastResponse());
-            }
+        sleep(2000);
+        if (SystemHost.getCommunications().getLastResponse().contains("OK")) {
+            Console.printOk("Serial TXRX Test Passed. No issues detected.");
+        } else {
+            Console.printErr("Serial TXRX Test Failed. Data recieved was not expected. I got: "
+                    + SystemHost.getCommunications().getLastResponse());
+        }
+        Console.printInfo("Checking BME280 Sensor");
 
-            Console.printInfo("====System Check Complete====");
-            Console.printOk("Resuming Subsystems...");
-            SystemHost.getAvionics().resumeSubsystem();
-            SystemHost.getCommunications().resumeSubsystem();
+        try {
+            BME280.main();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Console.printInfo("====System Check Complete====");
+        Console.printOk("Resuming Subsystems...");
+
+       
+
+
+
+            //SystemHost.getAvionics().resumeSubsystem();
+            //SystemHost.getCommunications().resumeSubsystem();
 
     }
     
